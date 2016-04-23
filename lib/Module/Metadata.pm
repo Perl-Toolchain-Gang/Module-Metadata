9b0d8a7d73d76ab53dded1ee2fca9e9a68662c73
Karen Etheridge <ether@cpan.org>
2016-04-23 11:53:03 +0100

    add line length to vim modeline

--- lib/Module/Metadata.pm
+++ lib/Module/Metadata.pm
@@ -1,5 +1,5 @@
 # -*- mode: cperl; tab-width: 8; indent-tabs-mode: nil; basic-offset: 2 -*-
-# vim:ts=8:sw=2:et:sta:sts=2
+# vim:ts=8:sw=2:et:sta:sts=2:tw=78
 package Module::Metadata;
 # ABSTRACT: Gather package and POD information from perl module files
 
