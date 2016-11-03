04b9b9271d16448eba059f5ddff6a1efbd67c770
Rob Halliday <robert.halliday@adestra.com>
2016-11-03 14:10:01 +0000

    Add $self->{inc} to @INC whilst evaluating version
    WWW::Scripter defines multiple packages in the same file, some of those
    packages contain a block of text that Module::Metadata tries to evaluate
    to get a version. The line that Module::Metadata tries to evaluate is
    "use WWW::Scripter; use WWW::Scripter; $VERSION = $WWW'Scripter'VERSION;"
    When using Carton to install WWW::Scripter this causes Module::Metadata
    to error with "Can't find module". If we add $self->{inc} to @INC before
    evaluating the version line then Perl looks in the right places for the
    WWW::Scripter module.

---
+++ t/lib/0_1/Bar.pm
@@ -0,0 +1,12 @@
+package Bar;
+our $VERSION = '0.1';
+
+package Bar::History;
+
+# adapted from WWW::Scripter
+<<'mldistwatch' if 0;
+use Bar; $VERSION = $Bar'VERSION;
+mldistwatch
+our $VERSION = $Bar'VERSION;
+
+1;
